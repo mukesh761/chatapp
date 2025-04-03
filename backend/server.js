2078490383d59fc1b55ce1661d7d9e19c0301a6a
mukesh761 <mukesh737462@gmail.com>
2025-04-03 08:11:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
-    origin: 'https://tution-fee-1.onrender.com',  // Replace with your frontend's URL (React dev server)
+    origin: 'https://chatapp-frontend-6h5u.onrender.com/',  // Replace with your frontend's URL (React dev server)
     credentials: true,
    // Allow credentials (cookies)
  };
